ca252b9a4c440998d197ea1774a9fc5e3d2c1588
soichiro <soichirom01124789@gmail.com>
2023-01-07 16:13:53 +0900

    OK

--- scripit.js
+++ scripit.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TCGMM2XTUTAEAZQJSXKBBO7B2KES5NMNTHLV4UI")
 console.log("Hello Symbol")
+console.log(`Your Address : ${address.plain()}`)
