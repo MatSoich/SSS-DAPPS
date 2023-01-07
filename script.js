5d8ff874c2653e87b2b2f62091d26db1d597acac
soichiro <soichirom01124789@gmail.com>
2023-01-07 17:34:10 +0900

    OK

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const symbol = require('/node_modules/symbol-sdk')
 const address = symbol.Address.createFromRawAddress("TCGMM2XTUTAEAZQJSXKBBO7B2KES5NMNTHLV4UI")
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
