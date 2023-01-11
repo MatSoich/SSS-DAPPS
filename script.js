e89600c0eef77dca866f0de0ff9caab28c08efbf
soichiro <soichirom01124789@gmail.com>
2023-01-11 09:45:25 +0900

    OK

--- script.js
+++ script.js
@@ -17,7 +17,62 @@ const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
 const accountHttp = repositoryFactory.createAccountRepository()
 const transactionHttp = repositoryFactory.createTransactionRepository()
 
-setTimeout(() => {
+
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
+
+accountHttp.getAccountInfo(address)
+    .toPromise()
+    .then((accountInfo) => {
+        for (let m of accountInfo.mosaics) {
+            if (m.id.id.toHex() === XYM_ID) {
+                const dom_xym = document.getElementById('wallet-xym')
+                dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
+            }
+        }
+    })
+
+const searchCriteria = {
+    group: symbol.TransactionGroup.Confirmed,
+    address,
+    pageNumber: 1,
+    pageSize: 20,
+    order: symbol.Order.Desc,
+}
+
+transactionHttp
+    .search(searchCriteria)
+    .toPromise()
+    .then((txs) => {
+        console.log(txs)
+        const dom_txInfo = document.getElementById('wallet-transactions')
+        for (let tx of txs.data) {
+            console.log(tx)
+            const dom_tx = document.createElement('div')
+            const dom_txType = document.createElement('div')
+            const dom_hash = document.createElement('div')
+
+            dom_txType.innerText = `Transaction Type : ${getTransactionType(tx.type)}`
+            dom_hash.innerText = `Transaction Hash : ${tx.transactionInfo.hash}`
+
+            dom_tx.appendChild(dom_txType)
+            dom_tx.appendChild(dom_hash)
+            dom_tx.appendChild(document.createElement('hr'))
+
+            dom_txInfo.appendChild(dom_tx)
+        }
+    })
+
+function getTransactionType(type) { // https://symbol.github.io/symbol-sdk-typescript-javascript/1.0.3/enums/TransactionType.html
+    if (type === 16724) return 'TRANSFER TRANSACTION'
+    return 'OTHER TRANSACTION'
+}
+
+
+
+
+/*setTimeout(() => {
 
     const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress)
 
@@ -70,7 +125,7 @@ function getTransactionType(type) {
     if (type === 16724) return 'TRANSFER TRANSACTION'
     return 'OTHER TRANSACTION'
 }
-
+*/
 
 
 function handleClick() {
